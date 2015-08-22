9fd42c9083b121e6f3140833a2d8516c0ba7c046
Vladimir de la Cruz <vladimirdlc@gmail.com>
2015-08-22 17:54:17 -0400

    Added player movement and enemy follow

---
+++ TheMonster/Assets/Scripts/Controls/TopDownController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TopDownController : MonoBehaviour {
+    public float speed;
+
+    void FixedUpdate()
+    {
+        mouseLook();
+
+    }
+
+    void mouseLook()
+    {
+        var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Quaternion rot = Quaternion.LookRotation(transform.position - mousePosition,
+                                                  Vector3.forward);
+
+        transform.rotation = rot;
+        transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
+        GetComponent<Rigidbody2D>().angularVelocity = 0;
+
+        float inputVertical = Input.GetAxis("Vertical");
+        float inputHorizontal = Input.GetAxis("Horizontal");
+
+        GetComponent<Rigidbody2D>().AddForce(gameObject.transform.up * speed * inputVertical * Time.deltaTime);
+        GetComponent<Rigidbody2D>().AddForce(gameObject.transform.right * speed * inputHorizontal * Time.deltaTime);
+    }
+}
---
+++ TheMonster/Assets/Scripts/Enemies/EnemyScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyScript : MonoBehaviour {
+    public float speed;
+    public Transform player;
+
+    void FixedUpdate()
+    {
+        /*float z = Mathf.Atan2((player.transform.position.y - transform.position.y), 
+            (player.transform.position.x - transform.position.x)) * Mathf.Rad2Deg - 90;
+
+        transform.eulerAngles = new Vector3(0, 0, z);
+
+        GetComponent<Rigidbody2D>().AddForce(gameObject.transform.up * speed);*/
+        transform.position += (player.transform.position - transform.position).normalized * speed * Time.deltaTime;
+    }
+}
